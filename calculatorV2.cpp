4112e0d2a49932caf77e01afbfb103ff2f09ac9a
Guess Me <45597250+czxel-itsme@users.noreply.github.com>
2019-04-12 22:17:59 +0700

    Update calculatorV2.cpp

--- calculatorV2.cpp
+++ calculatorV2.cpp
@@ -5,51 +5,54 @@ using namespace std;
 int main()
 {
 
-    char o;
-    float num1, num2, jumlah;
-
-    cout << "==========================" << endl;
-    cout << "KALKULATOR by ByteDevCode" << endl;
-    cout << "Pilihan : " << endl;
-    cout << "1. Tambah (+)" << endl;
-    cout << "2. Kurang (-)" << endl;
-    cout << "3. Kali (-)" << endl;
-    cout << "4. Bagi (/)" << endl;
-    cout << "===========================" << endl;
-
-    cout << "Masukkan Simbol : ";
-    cin >> o;
-
-    cout << endl;
-
-    cout << "Masukkan Angka 1 : ";
-    cin >> num1;
-    cout << "Masukkan Angka 2 : ";
-    cin >> num2;
-
-    cout << endl;
-
-    switch(o)
+    do
     {
-        case '+' :
-            cout << "Hasil " << num1 << " + " << num2 << " = " << num1 + num2;
-            break;
+        char o;
+        float num1, num2, jumlah;
 
-        case '-' :
-            cout << "Hasil " << num1 << " - " << num2 << " = " << num1 - num2;
-            break;
+        cout << "==========================" << endl;
+        cout << "KALKULATOR by ByteDevCode" << endl;
+        cout << "Pilihan : " << endl;
+        cout << "1. Tambah (+)" << endl;
+        cout << "2. Kurang (-)" << endl;
+        cout << "3. Kali (-)" << endl;
+        cout << "4. Bagi (/)" << endl;
+        cout << "===========================" << endl;
 
-        case '*' :
-            cout << "Hasil " << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
+        cout << "Masukkan Simbol : ";
+        cin >> o;
 
-        case '/' :
-            cout << "Hasil " << num1 << " / " << num2 << " = " << num1 / num2;
-            break;
+        cout << endl;
 
-        default :
-        break;
-    }
+        cout << "Masukkan Angka 1 : ";
+        cin >> num1;
+        cout << "Masukkan Angka 2 : ";
+        cin >> num2;
+
+        cout << endl;
+
+        switch(o)
+        {
+            case '+' :
+                cout << "Hasil " << num1 << " + " << num2 << " = " << num1 + num2;
+                break;
+
+            case '-' :
+                cout << "Hasil " << num1 << " - " << num2 << " = " << num1 - num2;
+                break;
+
+            case '*' :
+                cout << "Hasil " << num1 << " * " << num2 << " = " << num1 * num2;
+                break;
+
+            case '/' :
+                cout << "Hasil " << num1 << " / " << num2 << " = " << num1 / num2;
+                break;
+
+            default :
+            break;
+        }
+    } while (true);
 
     cout << endl;
 
